63b9c9ade19e8b700fb617bcf056f3f8d055f268
Danny Hollman <dannyhollman@gmail.com>
2020-04-13 02:00:12 -0400

    [INIT] Add files 0-6

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine(str + str + str);
+		Console.WriteLine(str.Substring(0, 9));
+        }
+}
