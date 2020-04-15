289f0629178af06a04b1a7c67c1ff75a35605a3c
Danny Hollman <dannyhollman@gmail.com>
2020-04-14 20:17:59 -0400

    [INIT] Add files 6-12

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Line
+{
+	public static void PrintDiagonal(int length)
+	{
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = 0; j <= i; j++)
+			{
+				if (j < i)
+					Console.Write(" ");
+				else if (j == i)
+					Console.Write("\\");
+			}
+			Console.WriteLine("");
+		}
+		Console.WriteLine("");
+	}
+}
---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int number = 1;
+
+		for (; number <= 100; number++)
+		{
+			if (number % 3 == 0 && number % 5 == 0)
+				Console.Write("FizzBuzz");
+			else if (number % 3 == 0)
+				Console.Write("Fizz");
+			else if (number % 5 == 0)
+				Console.Write("Buzz");
+			else
+				Console.Write(number);
+
+			if (number != 100)
+				Console.Write(" ");
+		}
+		Console.WriteLine("");
+	}
+}
