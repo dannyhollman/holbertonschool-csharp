3efbc96aca801a0ddb17f6450c877b0ab28e65f6
Danny Hollman <dannyhollman@gmail.com>
2020-04-16 17:24:19 -0400

    [UPDATE] Check if index is in range

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -6,7 +6,7 @@ class List
 {
 	public static List<int> DeleteAt(List<int> myList, int index)
 	{
-		if (index >= myList.Count)
+		if (index >= myList.Count || index < 0)
 		{
 			Console.WriteLine("Index out of range");
 			return myList;
