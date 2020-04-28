36bab69f6e8b2ae51c3e1b40cf3c09463773256a
Danny Hollman <dannyhollman@gmail.com>
2020-04-27 20:59:58 -0400

    [UPDATE] Add null check

--- 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -5,6 +5,8 @@ class LList
 {
 	public static int Pop(LinkedList<int> myLList)
 	{
+		if(myLList == null || myLList.Count == 0)
+			return 0;
 		int val = myLList.First.Value;
 		myLList.RemoveFirst();
 		return val;
