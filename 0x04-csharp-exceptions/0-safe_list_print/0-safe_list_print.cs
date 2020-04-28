c9b2c8e7eef86c2cf1fed0dd363758504a914f53
Danny Hollman <dannyhollman@gmail.com>
2020-04-28 14:19:05 -0400

    [INIT] Add all files

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static int SafePrint(List<int> myList, int n)
+	{
+		int i;
+		for(i = 0; i < n; i++)
+		{
+			try
+			{
+				Console.WriteLine(myList[i]);
+			}
+			catch
+			{
+				return i;
+			}
+		}
+		return i;
+	}
+}
