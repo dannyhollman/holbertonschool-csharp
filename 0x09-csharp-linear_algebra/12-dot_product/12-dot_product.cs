a44ee93e624db9b4fc5db52a2bd01866d2274e6c
Danny Hollman <dannyhollman@gmail.com>
2020-05-27 16:21:17 -0400

    [UPDATE] Fix error checking, add comments

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 
+/// <summary> VectorMath class </summary>
 class VectorMath
 {
+	/// <summary> Find dot product of vectors </summary>
 	public static double DotProduct(double[] vector1, double[] vector2)
 	{
-		if (vector1.Length > 3 || vector2.Length > 3 || vector1.Length != vector2.Length)
+		if (vector1.Length != 3 && vector1.Length != 2)
+			return -1;
+		if (vector2.Length != 3 && vector2.Length != 2)
+			return -1;
+		if (vector1.Length != vector2.Length)
 			return -1;
 
 		double x, y, z, dot = 0;
