80501a113c946f7ffdd82966b506d7555f8c9220
Danny Hollman <dannyhollman@gmail.com>
2020-05-27 16:11:25 -0400

    [UPDATE] Fix error checking

--- 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 
+/// <summary> VectorMath class </summary>
 class VectorMath
 {
+	/// <summary> Multiply two vectors </summary>
 	public static double[] Multiply(double[] vector, double scalar)
 	{
 		double[] error = { -1, -1 };
-		if (vector.Length > 3)
+		if (vector.Length != 3 && vector.Length != 2)
 			return error;
 
 		for (int i = 0; i < vector.Length; i++)
