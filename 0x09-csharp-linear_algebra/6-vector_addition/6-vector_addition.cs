67d8a3a4b21bace34801b26134e85ba1899d00ac
Danny Hollman <dannyhollman@gmail.com>
2020-05-27 15:30:12 -0400

    [UPDATE] Fix error checking, add xml

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
+/// <summary> VectorMath class </summary>
 class VectorMath
 {
+	/// <summary> Add two vectors </summary>
 	public static double[] Add(double[] vector1, double[] vector2)
 	{
 		double[] error = { -1, -1 };
-		if (vector1.Length > 3 || vector2.Length > 3 || vector1.Length != vector2.Length)
+		if (vector1.Length > 3 || vector1.Length < 2)
 			return error;
+		if (vector2.Length > 3 || vector2.Length < 2)
+			return error;
+		if (vector1.Length != vector2.Length)
 
 		for (int i = 0; i < vector1.Length; i++)
 			vector1[i] += vector2[i];
