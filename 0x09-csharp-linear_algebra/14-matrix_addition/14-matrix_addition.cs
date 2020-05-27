90b85e7b38c68d0bd7c11670c8df02bf5fb01041
Danny Hollman <dannyhollman@gmail.com>
2020-05-27 16:37:38 -0400

    [UPDATE] Fix error checking

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -5,19 +5,20 @@ class MatrixMath
 	public static double[,] Add(double[,] matrix1, double[,] matrix2)
 	{
 		double[,] error = {{ -1 }};
-		if (matrix1.GetLength(0) < 2 || matrix1.GetLength(1) < 2)
+		if (matrix1.GetLength(0) != 2 && matrix1.GetLength(0) != 3)
 			return error;
-		if (matrix2.GetLength(0) < 2 || matrix2.GetLength(1) < 2)
+		if (matrix2.GetLength(0) != 2 && matrix2.GetLength(0) != 2)
 			return error;
 		if (matrix1.GetLength(0) != matrix2.GetLength(0))
 			return error;
 		if (matrix1.GetLength(1) != matrix2.GetLength(1))
 			return error;
 
+		double[,] sum = new double[matrix1.Length, matrix1.Length];
 		for (int i = 0; i < matrix1.GetLength(0); i++)
 			for (int j = 0; j < matrix1.GetLength(1); j++)
-				matrix1[i, j] += matrix2[i, j];
+				sum[i, j] = matrix1[i, j] + matrix2[i, j];
 
-		return matrix1;
+		return sum;
 	}
 }
