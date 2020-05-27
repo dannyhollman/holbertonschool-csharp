e52671d3442813502567ce07432796234a1bc44c
Danny Hollman <dannyhollman@gmail.com>
2020-05-27 16:25:40 -0400

    [UPDATE] Simplify code

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -13,22 +13,11 @@ class VectorMath
 		if (vector1.Length != vector2.Length)
 			return -1;
 
-		double x, y, z, dot = 0;
+		double dot = 0;
 
-		if (vector1.Length == 3)
-		{
-			x = vector1[0] * vector2[0];
-			y = vector1[1] * vector2[1];
-			z = vector1[2] * vector2[2];
-			dot = x + y + z;
-		}
+		for (int i = 0; i < vector1.Length; i++)
+			dot += vector1[i] * vector2[i];
 
-		if (vector2.Length == 2)
-		{
-			x = vector1[0] * vector2[0]; 
-			y = vector2[1] * vector2[1];
-			dot = x + y;
-		}
 		return dot;
 	}
 }
