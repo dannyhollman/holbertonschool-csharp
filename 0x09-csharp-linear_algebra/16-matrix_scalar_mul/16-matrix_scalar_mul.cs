118dab9cb82ee03ca2bab0ab501cd67b7092b85a
Danny Hollman <dannyhollman@gmail.com>
2020-05-27 16:46:41 -0400

    [UPDATE] Fix error checking

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -5,13 +5,14 @@ class MatrixMath
 	public static double[,] MultiplyScalar(double[,] matrix, double scalar)
 	{
 		double[,] error = {{ -1 }};
-		if (matrix.GetLength(0) > 3 || matrix.GetLength(0) < 1)
+		if (matrix.GetLength(0) != 3 && matrix.GetLength(0) != 2)
 			return error;
-		if (matrix.GetLength(1) > 3 || matrix.GetLength(1) < 1)
+		if (matrix.GetLength(1) != 3 && matrix.GetLength(1) != 2)
 			return error;
+		double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
 		for (int i = 0; i < matrix.GetLength(0); i++)
 			for (int j = 0; j < matrix.GetLength(1); j++)
-				matrix[i, j] *= scalar;
-		return matrix;
+				result[i, j] = matrix[i, j] * scalar;
+		return result;
 	}
 }
