0ae4c4892deb1ed9d3eb5e4cbfd2582a13d42625
Danny Hollman <dannyhollman@gmail.com>
2020-05-27 15:22:02 -0400

    [UPDATE] Add comments, check vector length

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 
+/// <summary> VectorMath class </summary>
 class VectorMath
 {
+	/// <summary> Calculates magnitude of vector </summary>
 	public static double Magnitude(double[] vector)
 	{
+		if (vector.Length > 3 || vector.Length < 2)
+			return -1;
 		double length = 0;
 		foreach(double x in vector)
 			length += Math.Pow(x, 2);
