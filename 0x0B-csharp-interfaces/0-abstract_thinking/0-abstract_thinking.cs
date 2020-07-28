55822317364a29331f77938119139770eefa4aac
Danny Hollman <dannyhollman@gmail.com>
2020-07-28 18:48:51 -0400

    [UPDATE] Add comments

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 
+/// <summary> Base class </summary>
 public abstract class Base
 {
+	/// <summary> name </summary>
 	public string name;
-
+	/// <summary> Override ToString </summary>
 	public override string ToString()
 	{
 		return name + " is a " + this.GetType();
